02a8fbe74030116094e802ee7a5ec003f6b933af
markuz899 <markuz.89@hotmail.it>
2023-11-26 00:22:14 +0100

    [feat] - init sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Sidebar from "./components/Sidebar";
+import styled from "styled-components";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <ContentApp>
+      <Sidebar />
+      <Content>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </Content>
+    </ContentApp>
+  );
 }
 
-export default App
+export default App;
+
+const ContentApp = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+const Content = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px 60px;
+`;
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+interface IMenuItem {
+  isOpen?: boolean;
+}
+
+const menuItems = [
+  { title: "Dashboard", icon: "" },
+  { title: "Users", icon: "" },
+  { title: "Cloud services", icon: "" },
+  { title: "Usage data", icon: "" },
+  { title: "Server list", icon: "" },
+];
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <Sider className={`sidebar ${!isOpen ? "sidebar_closed" : ""}`}>
+      <button className={"sidebar_button"} onClick={() => setIsOpen(!isOpen)}>
+        ---
+      </button>
+      <ul>
+        {menuItems.map((item) => (
+          <MenuItem key={item.title} isOpen={isOpen}>
+            <li>
+              <div className="item">
+                ICON
+                <span>{item.title}</span>
+              </div>
+            </li>
+          </MenuItem>
+        ))}
+      </ul>
+    </Sider>
+  );
+};
+
+export default Sidebar;
+
+const Sider = styled.div`
+  min-width: 250px;
+  background: darkslategrey;
+  padding: 20px;
+  color: white;
+  transition: 0.2s ease-in;
+
+  &.sidebar_closed {
+    min-width: 40px;
+  }
+
+  .sidebar_button {
+    background: transparent;
+    padding: 10px;
+    border: 1px solid white;
+    border-radius: 4px;
+    color: white;
+    transition: 0.3s linear;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    overflow: hidden;
+  }
+`;
+
+const MenuItem: any = styled.div<IMenuItem>`
+  .item {
+    margin: 30px 10px;
+    display: flex;
+    height: 20px;
+    span {
+      white-space: nowrap;
+      width: 0;
+      display: ${(p) => (p.isOpen ? "block" : "none")};
+    }
+  }
+`;
